a1c182a505f46ff3a90d7d6abd9d1732b0a4f7b8
Animesh Singh <anymeshsingh@gmail.com>
2019-02-20 22:17:09 +0530

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,5 @@
+module.exports = {
+    APPLICATION_ID: process.env.APPLICATION_ID || 'eadba9d2b8c47952bc020959c6bcb1366dee1ade9a786fd144478824271645db',
+    SECRET: process.env.SECRET || 'e576f73727070fd41056a7ea6ba21d113de90f8887e29df439b06f9b2f06ac47',
+    CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000'
+}
